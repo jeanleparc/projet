7852081e98030158e39c0f8fc3b10167b804e1b1
Dr.-Ing. Franklin Tchakounté <tchafros@gmail.com>
2017-03-29 14:12:40 +0100

    Classe minimale

--- JavaApplication29j/src/javaapplication29j/JavaApplication29j.java
+++ JavaApplication29j/src/javaapplication29j/JavaApplication29j.java
@@ -12,9 +12,7 @@ package javaapplication29j;
  */
 public class JavaApplication29j {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         // TODO code application logic here
     }
